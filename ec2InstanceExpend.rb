331876385fb70e8390b6c67a6dd18cce0ff4b30b
ken_goto <ken_goto@cybird.co.jp>
2013-11-12 19:37:33 +0900

    instance_id,nameでクローン元を指定できるようにした

--- ec2InstanceExpend.rb
+++ ec2InstanceExpend.rb
@@ -4,5 +4,38 @@ require 'aws_include.rb'
 # コマンドライン引数受取
 require 'optparse'
 
-# デフォルト値を設定する
-get_instance_id("a")
+# 引数を解析する
+OptionParser.new do |opts|
+    begin
+        # オプション情報を設定する
+        opts = OptionParser.new
+        opts.on('-i instance_id',
+                '--instance-id instance_id',
+                "EC2のInstance Idを指定") { 
+            |v| config[:instance_id] = v 
+        }
+        opts.on('-n name',
+                '--name name',
+                "TagのName要素を指定") {
+            |v| config[:name] = v
+        }
+
+        opts.parse!(ARGV)
+
+    rescue => e
+        puts opts.help
+        puts
+        puts e.message
+        exit 1
+    end
+end
+
+if !config[:instance_id].nil? then
+    input_instance_id = config[:instance_id]
+elsif !config[:name].nil? then
+    input_instance_id = get_instance_id(config[:name])
+else
+    input_id = input("クローン元のEC2インスタンスのidを入力して下さい : ")
+end
+
+
