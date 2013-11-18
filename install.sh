35b5802a0014249a6a42c0c2b1a382d96496685d
ken_goto <ken_goto@cybird.co.jp>
2013-11-18 14:08:54 +0900

    必要項目を入力させるようにした

--- install.sh
+++ install.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
+# 入力
+echo -n "input aws_access_key_id > "
+read KEY_ID
+echo -n "input aws_secret_access_key > "
+read ACCESS_KEY
+echo -n "input region > "
+read REASION
+
 wget http://peak.telecommunity.com/dist/ez_setup.py
 python ez_setup.py
 easy_install pip
 pip install awscli
 mkdir ~/.aws
 touch ~/.aws/config
-printf "[default]\r\n" >> ~/.aws/config
-printf "aws_access_key_id = AKIAJSG6AJ3H6KPTXMFQ\r\n" >> ~/.aws/config
-printf "aws_secret_access_key = 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\r\n" >> ~/.aws/config
-printf "region = ap-northeast-1\r\n" >> ~/.aws/config
+printf "[default]\n" >> ~/.aws/config
+#printf "aws_access_key_id = AKIAJSG6AJ3H6KPTXMFQ\r\n" >> ~/.aws/config
+#printf "aws_secret_access_key = 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\r\n" >> ~/.aws/config
+#printf "region = ap-northeast-1\r\n" >> ~/.aws/config
+printf "aws_access_key_id = %03d\n"$KEY_ID >> ~/.aws/config
+printf "aws_secret_access_key = %03d\n"$ACCESS_KEY >> ~/.aws/config
+printf "region = %03d\n"$REASION >> ~/.aws/config
 aws ec2 describe-instances --dry-run
 
 # jq インストール
